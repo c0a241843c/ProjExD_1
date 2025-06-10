b8b80dc007a2189a1fa60e60c1a441679c5da384
c0a241843c <c0a241843c@edu.teu.ac.jp>
2025-06-10 15:44:56 +0900

    背景を繋げる

--- pygame_image.py
+++ pygame_image.py
@@ -17,11 +17,13 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0-tmr, 0])
+        x  = -tmr
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img, [x+1600, 0])
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
